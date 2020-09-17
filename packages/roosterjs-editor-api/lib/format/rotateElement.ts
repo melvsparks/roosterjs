32afe765396313e52b35167353e85e56878f0a90
Melina Sparks <melspa@microsoft.com>
2020-09-17 10:25:59 -0700

    Ensure multiple images flip - still not quite right

--- packages/roosterjs-editor-api/lib/format/rotateElement.ts
+++ packages/roosterjs-editor-api/lib/format/rotateElement.ts
@@ -1,6 +1,7 @@
 import { ChangeSource } from 'roosterjs-editor-types';
 import { Editor } from 'roosterjs-editor-core';
 
+const ROTATE_CANVAS_ID = "rotateImageCanvasId";
 /**
  * Rotate an element visually
  * @param editor The editor instance
@@ -10,24 +11,34 @@ import { Editor } from 'roosterjs-editor-core';
 export default function rotateElement(editor: Editor, element: HTMLElement, angle: number): void {
     if (element) {
         editor.addUndoSnapshot(() => {
-            let canvas = <HTMLCanvasElement>editor.getDocument().getElementById('rotateImageCanvas');
+            let canvas = <HTMLCanvasElement>editor.getDocument().getElementById(ROTATE_CANVAS_ID);
             const image = <HTMLImageElement>element; // TODO: change name of api
 
             if (!canvas) {
                 canvas = editor.getDocument().createElement("canvas");
+                canvas.id = ROTATE_CANVAS_ID;
                 const imageParent = image.parentElement;
                 canvas.appendChild(image);
                 imageParent.appendChild(canvas);
-                canvas.id = "rotateImageCanvas";
-                const maxWidth = Math.sqrt(Math.pow(image.width, 2) + Math.pow(image.height, 2));
-                canvas.width = maxWidth;
-                canvas.height = maxWidth;
+                setCanvasDimensions(image.width, image.height, canvas);
+            } else if (image != canvas.firstElementChild) {
+                const oldImage = canvas.firstElementChild;
+                const imageParent = image.parentElement;
+                imageParent.appendChild(oldImage);
+                canvas.appendChild(image);
+                setCanvasDimensions(image.width, image.height, canvas);
             }
 
             const context = canvas.getContext("2d");
             rotateWithinCanvas(image, context, canvas, angle);
         }, ChangeSource.Format);
     }
+}
+
+function setCanvasDimensions(imageWidth: number, imageHeight: number, canvas: HTMLCanvasElement) {
+    const maxWidth = Math.sqrt(Math.pow(imageWidth, 2) + Math.pow(imageHeight, 2));
+    canvas.width = maxWidth;
+    canvas.height = maxWidth;
 }
 
 function rotateWithinCanvas(image: HTMLImageElement, context: CanvasRenderingContext2D, canvas: HTMLCanvasElement, angle: number) {
